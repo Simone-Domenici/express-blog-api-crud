c6f9715738a29dbb7f74afd159276cf6783f7859
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-20 15:39:28 +0100

    ADD the logic in the controller file and moved the file with posts on data

---
+++ controllers/postController.js
@@ -0,0 +1,65 @@
+const posts = require('../data/posts.js')
+
+// Index
+function index(req,res) {
+    console.log('Lista dei post')
+    let filteredPosts = posts
+
+	if (req.query.tag) {
+		filteredPosts = posts.filter((post) => {
+			return post.tags.includes(req.query.tag.toLowerCase())
+		})
+}
+// Show
+function show(req,res) {
+    const id = parseInt(req.params.id)
+	console.log(`Ecco il post con id: ${id}`)
+
+	const post = posts.find((post) => post.id === id)
+	let result = post
+
+	if (!post) {
+		console.log('Post non trovato')
+
+		res.status(404)
+		result = {
+			error: 'Post not found',
+			message: 'Il post non è stato trovato.',
+		}
+	}
+
+	res.json(result)
+}
+// Store
+function store(req,res) {
+    res.send('Creazione del post')
+}
+// Update
+function update(req,res) {
+    const slug = req.params.slug
+    res.send(`Aggiornamento del post ${slug}`)
+}
+// Modify
+function modify(req,res) {
+    res.send(`Modifica del post ${slug}`)
+}
+// Destroy
+function destroy(req,res) {
+    const id = parseInt(req.params.id)
+	console.log(`Elimino il post con id: ${id}`)
+
+	const postsIndex = posts.findIndex((post) => post.id === id)
+
+	if (postsIndex === -1) {
+		res.status(404)
+
+		return res.json({
+			error: 'Post not found',
+			message: 'Il post non è stato trovato.',
+		})
+	}
+	posts.splice(postsIndex, 1)
+	res.sendStatus(204)
+}
+
+module.exports = { index, show, store, update, modify, destroy }
--- routers/posts.js
+++ routers/posts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const posts = require('../posts.js')
+const posts = require('../data/posts.js')
 
 // Index
 router.get('/' ,(req,res) => {
