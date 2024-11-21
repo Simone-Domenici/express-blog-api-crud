f23dc5c9eae33825e7121f9b55945ed8dfa2c5bc
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-21 16:58:55 +0100

    ADD modify function

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,19 +2,19 @@ const posts = require('../data/posts.js')
 let lastIndex = posts.at(-1).id
 
 // Index
-function index(req,res) {
+function index(req, res) {
     console.log('Lista dei post')
     let filteredPosts = posts
 
-	if (req.query.tag) {
-		filteredPosts = posts.filter((post) => {
-			return post.tags.includes(req.query.tag.toLowerCase())
-		})
-    }  
+    if (req.query.tag) {
+        filteredPosts = posts.filter((post) => {
+            return post.tags.includes(req.query.tag.toLowerCase())
+        })
+    }
     res.json(filteredPosts)
 }
 // Show
-function show(req,res) {
+function show(req, res) {
     const identifier = req.params.identifier;
     console.log(`Mostro il post con id: ${identifier}`)
 
@@ -30,158 +30,182 @@ function show(req,res) {
     const post = findPost(identifier);
     let result = post
 
-	if (!post) {
-		console.log('Post non trovato')
+    if (!post) {
+        console.log('Post non trovato')
 
-		res.status(404)
-		result = {
-			error: 'Post not found',
-			message: 'Il post non è stato trovato.',
-		}
-	}
+        res.status(404)
+        result = {
+            error: 'Post not found',
+            message: 'Il post non è stato trovato.',
+        }
+    }
 
-	res.json(result)
+    res.json(result)
 }
 // Store
-function store(req,res) {
+function store(req, res) {
     console.log('Creazione del post')
     console.log(req.body)
     const { title, content, image, tags } = req.body
 
-	const errors = validate(req)
+    const errors = validate(req)
 
-	if (errors.length) {
+    if (errors.length) {
 
-		res.status(400)
+        res.status(400)
 
-		return res.json({
-			error: 'Invalid request',
-			messages: errors,
-		})
-	}
+        return res.json({
+            error: 'Invalid request',
+            messages: errors,
+        })
+    }
 
-	lastIndex++
+    lastIndex++
     const slug = createSlug(title)
 
-	const post = {
-		id: lastIndex,
-		title,
-        slug, 
-		content,
-		image,
+    const post = {
+        id: lastIndex,
+        title,
+        slug,
+        content,
+        image,
         tags: [tags]
-	}
+    }
 
-	posts.push(post)
-	res.status(201).send(post)
+    posts.push(post)
+    res.status(201).send(post)
 }
 
 // Update
-function update(req,res) {
+function update(req, res) {
     const id = parseInt(req.params.id)
     console.log(`Aggiornamento del post ${id}`)
 
-	const errors = validate(req)
+    const errors = validate(req)
 
-	if (errors.length) {
-		res.status(400)
+    if (errors.length) {
+        res.status(400)
 
-		return res.json({
-			error: 'Invalid request',
-			messages: errors,
-		})
-	}
+        return res.json({
+            error: 'Invalid request',
+            messages: errors,
+        })
+    }
 
-	const post = posts.find((post) => post.id === id)
+    const post = posts.find((post) => post.id === id)
 
-	if (!post) {
-		res.status(404)
+    if (!post) {
+        res.status(404)
 
-		return res.json({
-			error: 'Post not found',
-			message: 'Il post non è stato trovato.',
-		})
-	}
+        return res.json({
+            error: 'Post not found',
+            message: 'Il post non è stato trovato.',
+        })
+    }
 
-	const { title, image, content, tags } = req.body
+    const { title, image, content, tags } = req.body
 
-	post.title = title
+    post.title = title
     post.slug = createSlug(title)
-	post.content = content
-	post.image = image
+    post.content = content
+    post.image = image
     post.tags = tags
 
-	res.json(post)
+    res.json(post)
 }
 // Modify
-function modify(req,res) {
-    res.send(`Modifica del post ${id}`)
-}
+function modify(req, res) {
+    const id = parseInt(req.params.id)
+    console.log(`Modifica del post ${id}`)
 
-// Destroy
-function destroy(req,res) {
-	const identifier = req.params.identifier;
-    console.log(`Elimino il post con id: ${identifier}`)
+    const post = posts.find((post) => post.id === id)
 
-    const findPost = (idOrSlug) => {
-        if (!isNaN(idOrSlug)) {
-            // Cerca per ID
-            return posts.find(post => post.id === parseInt(idOrSlug));
-        } else {
-            // Cerca per slug
-            return posts.find(post => post.slug === idOrSlug.toLowerCase());
-        }
+    if (!post) {
+        res.status(404)
+
+        return res.json({
+            error: 'Post not found',
+            message: 'Il post non è stato trovato.',
+        })
+    }
+
+    const { title, image, content, tags } = req.body
+
+    if (title) {
+    post.title = title
+    post.slug = createSlug(title)
+    }
+    if (content) post.content = content
+    if (image) post.image = image
+    if (tags) post.tags = tags
+
+    res.json(post)
+    }
+
+    // Destroy
+    function destroy(req, res) {
+        const identifier = req.params.identifier;
+        console.log(`Elimino il post con id: ${identifier}`)
+
+        const findPost = (idOrSlug) => {
+            if (!isNaN(idOrSlug)) {
+                // Cerca per ID
+                return posts.find(post => post.id === parseInt(idOrSlug));
+            } else {
+                // Cerca per slug
+                return posts.find(post => post.slug === idOrSlug.toLowerCase());
+            }
         };
-    const post = findPost(identifier);
-    const postsIndex = posts.indexOf(post)
+        const post = findPost(identifier);
+        const postsIndex = posts.indexOf(post)
 
-	if (postsIndex === -1) {
-		res.status(404)
+        if (postsIndex === -1) {
+            res.status(404)
 
-		return res.json({
-			error: 'Post not found',
-			message: 'Il post non è stato trovato.',
-		})
-	}
-	posts.splice(postsIndex, 1)
-	res.sendStatus(204)
-}
+            return res.json({
+                error: 'Post not found',
+                message: 'Il post non è stato trovato.',
+            })
+        }
+        posts.splice(postsIndex, 1)
+        res.sendStatus(204)
+    }
 
-module.exports = { index, show, store, update, modify, destroy }
+    module.exports = { index, show, store, update, modify, destroy }
 
-// Funzioni
+    // Funzioni
 
-function validate(req) {
-	const { title, content, image, tags } = req.body
+    function validate(req) {
+        const { title, content, image, tags } = req.body
 
-	const errors = []
+        const errors = []
 
-	if (!title) {
-		errors.push('Name is required')
-	}
+        if (!title) {
+            errors.push('Name is required')
+        }
 
-	if (!image) {
-		errors.push('Image is required')
-	}
+        if (!image) {
+            errors.push('Image is required')
+        }
 
-	if (!content) {
-		errors.push('Ingredients is required')
-	}
+        if (!content) {
+            errors.push('Ingredients is required')
+        }
 
-    if (!tags) {
-		errors.push('Tag is required')
-	}
+        if (!tags) {
+            errors.push('Tag is required')
+        }
 
-	return errors
-}
+        return errors
+    }
 
 
-function createSlug(title) {
-    // Rimuovi caratteri speciali e accent
-    const slug = title.toLowerCase()
-                        .replace(/[^\w\s-]/g, '') // Rimuovo caratteri non alfanumerici, spazi e trattini
-                        .replace(/\s+/g, '-') // Sostituisco spazi multipli con un singolo trattino
-                        .replace(/-+/g, '-'); // Rimuovo trattini multipli
+    function createSlug(title) {
+        // Rimuovi caratteri speciali e accent
+        const slug = title.toLowerCase()
+            .replace(/[^\w\s-]/g, '') // Rimuovo caratteri non alfanumerici, spazi e trattini
+            .replace(/\s+/g, '-') // Sostituisco spazi multipli con un singolo trattino
+            .replace(/-+/g, '-'); // Rimuovo trattini multipli
 
-    return slug;
-}
+        return slug;
+    }
