d10664842c2606f60d0d57f0143f73475d730dee
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-22 15:55:16 +0100

    ADD errorsHandler middleware

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express()
 const port = 3000
 const postsRouter = require('./routers/posts.js')
 const notFound = require('./middlewares/notFound.js')
+const errorsHandler = require('./middlewares/errorsHandler.js')
 
 app.use(express.static('public'));
 app.use(express.json())
 app.use('/posts' , postsRouter)
 
 
+app.use(errorsHandler)
 app.use(notFound)
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
