c512a856adc4c7c59139b6d53dbf3329fc78e3cf
Simone-Domenici <domenici.simone00@gmail.com>
2024-11-22 15:21:54 +0100

    ADD notFound middleware

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const postsRouter = require('./routers/posts.js')
+const notFound = require('./middlewares/notFound.js')
 
 app.use(express.static('public'));
 app.use(express.json())
 app.use('/posts' , postsRouter)
 
 
-
+app.use(notFound)
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
